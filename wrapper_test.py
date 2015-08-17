b9f98239644cf64ce9c464a849c033635852ccf3
Jamie Tam <jamietam@umich.edu>
2015-08-17 15:18:23 -0400

    keep error output in /home/jamietam/ directory

--- wrapper_test.py
+++ wrapper_test.py
@@ -15,10 +15,10 @@ scen2 = (1,1,1,0.75,0.75,0.75)
 scenario_set = (scen1,scen2)
 
 for j in xrange(numThreads):
-   #cmd3 = "sudo python /home/jamietam/scenarios_parallel_%s/test_Run_4000_scenarios_test.py %s > test-%s 2>> std.out-test-%s &" % (str(j), str(j), str(j), str(j))
+   #cmd3 = "sudo python /home/jamietam/scenarios_parallel_%s/test_Run_4000_scenarios_test.py %s > /home/jamietam/test-%s 2>> /home/jamietam/std.out-test-%s &" % (str(j), str(j), str(j), str(j))
    cmd3 = "sudo python /home/jamietam/web-interface-shg-policy/Run_4000_scenarios_test.py %s " % str(j)
    cmd3 = cmd3+"%s %s %s %s %s %s> " % scenario_set[j]
-   cmd3 = cmd3+"test-%s 2>> std.out-test-%s &" % (str(j), str(j))
+   cmd3 = cmd3+"/home/jamietam/test-%s 2>> /home/jamietam/std.out-test-%s &" % (str(j), str(j))
 
    out = "echo submitting run %s" % str(j)   
    os.system(out)

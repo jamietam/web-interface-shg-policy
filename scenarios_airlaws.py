2e215b763166acaad4fbc94826f678d50e800131
Jamie Tam <jamietam@umich.edu>
2018-09-30 10:04:53 -0400

    arilaws and tcexp parallelized

--- scenarios_airlaws.py
+++ scenarios_airlaws.py
@@ -10,176 +10,24 @@ import time
 dirweb = '/home/jamietam/web-interface-shg-policy/'# Directory contains age effects files
 dirresults = '/home/jamietam/airlaws_results/'
 
-scenarioDict = {'0':{'Iwp':[0],
-                      'Ir':[0],
-                      'Ib':[0],
-                      'pacwp':[0.0],
-                      'pacb':[0.0],
-                      'pacr':[0.0],
-                      'years':range(2016,2021)
-                      },
-               '1':{'Iwp':[1],
-                      'Ir':[0],
-                      'Ib':[0],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00],
-                      'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-               '2':{'Iwp':[0],
-                      'Ir':[1],
-                      'Ib':[0],
-                     'pacwp':[0.00],
-                      'pacb':[0.00,0.25,0.50,0.75,1.00],
-                      'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-               '3':{'Iwp':[0],
-                      'Ir':[0],
-                      'Ib':[1],
-                      'pacwp':[0.00],
-                      'pacb':[0.00],
-                      'pacr':[0.00,0.25,0.50,0.75,1.00],
-                      'years':range(2016,2021)
-                      } ,
-               '4':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[0],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
-                      'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-               '5':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[0],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50, 0.75,1.00],
-                      'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-
-               '6':{'Iwp':[1],
-                      'Ir':[0],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00],
-                      'pacr':[0.00,0.25],
-                      'years':range(2016,2021)
-                      },
-               '7':{'Iwp':[1],
-                      'Ir':[0],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00],
-                      'pacr':[0.50,0.75,1.00],
-                      'years':range(2016,2021)
-                      },
-               '8':{'Iwp':[0],
+scenarioDict = {'19':{'Iwp':[1],
                       'Ir':[1],
                       'Ib':[1],
                       'pacwp':[0.00],
-                      'pacb':[0.00,0.25,0.50, 0.75,1.00],
-                      'pacr':[0.00,0.25],
-                      'years':range(2016,2021)
-                      },
-               '9':{'Iwp':[0],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00],
-                      'pacb':[0.00,0.25,0.50, 0.75,1.00],
-                      'pacr':[0.50,0.75,1.00],
-                      'years':range(2016,2021)
-                      },
-               '10':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
                       'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-               '11':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50, 0.75,1.00],
-                      'pacr':[0.00],
-                      'years':range(2016,2021)
-                      },
-                '12':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
-                      'pacr':[0.25],
-                      'years':range(2016,2021)
-                      },
-               '13':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50, 0.75,1.00],
-                      'pacr':[0.25],
-                      'years':range(2016,2021)
-                      },
-                '14':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
-                      'pacr':[0.50],
-                      'years':range(2016,2021)
-                      },
-               '15':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50, 0.75,1.00],
-                      'pacr':[0.50],
-                      'years':range(2016,2021)
-                      },
-                '16':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
-                      'pacr':[0.75],
-                      'years':range(2016,2021)
-                      },
-               '17':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50, 0.75,1.00],
-                      'pacr':[0.75],
-                      'years':range(2016,2021)
-                      },
-                '18':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.00,0.25],
-                      'pacr':[1.00],
-                      'years':range(2016,2021)
-                      },
-                '19':{'Iwp':[1],
-                      'Ir':[1],
-                      'Ib':[1],
-                      'pacwp':[0.00,0.25,0.50,0.75,1.00],
-                      'pacb':[0.50,0.75,1.00],
-                      'pacr':[1.00],
-                      'years':range(2016,2021)
+                      'pacb':[0.00],
+                      'years':[2016]
                       }
-
                 }
 
 ## CODE CURRENTLY SET UP TO DISTRIBUTE PARAMETER COMBOS TO EACH CORE, NOT EACH SCENARIO TO EACH CORE
 
 def policyrun (Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,directory):
     combos = product(Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set)    
-    numscenarios = 0 
-    for scen in list(combos):
+    numscenarios = 0
+    combos_list=list(combos)
+    print "We have {} scenarios".format(len(combos_list))
+    for scen in combos_list:
 	dirsim = '/home/jamietam/shg-policy-module_parallel_{}/'.format(directory) # Directory contains 'policy_shg.py'
 	dirinputs = dirsim + 'inputs/' # Directory contains 'policies.csv' and 'demographics.csv'
     	# Create policy inputs file
@@ -191,7 +39,7 @@ def policyrun (Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,direc
     	cmd1=cmd1+dirinputs+"policies.csv"
     	os.system(cmd1)
     	os.chdir(dirinputs)
-    	os.system("cp demographics_males_200000.csv demographics.csv")
+    	os.system("cp demographics_males_500000.csv demographics.csv")
     	os.chdir(dirsim)
 
     	runitM = "python policy_shg.py >> ../logM{0}.txt 2>> ../errorM{0}.txt".format(directory) ### NAME THESE AS THE SCENARIO
@@ -206,7 +54,7 @@ def policyrun (Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,direc
     	cmd3=cmd3+dirinputs+"policies.csv"
     	os.system(cmd3)
     	os.chdir(dirinputs)
-    	os.system("cp demographics_females_200000.csv demographics.csv")
+    	os.system("cp demographics_females_500000.csv demographics.csv")
     	os.chdir(dirsim)
 
     	runitF = "python policy_shg.py >> ../logF{0}.txt 2>> ../errorF{0}.txt".format(directory)
@@ -222,7 +70,7 @@ def policyrun (Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,direc
 
 if __name__ == '__main__':
     # cpus = mp.cpu_count()
-    pool = mp.Pool(processes=20)
+    pool = mp.Pool(processes=1)
     for key, scenario in scenarioDict.items():
         directory = key
         print("ITERATION: "+ directory)
@@ -234,7 +82,6 @@ if __name__ == '__main__':
         pacr_set = scenario['pacr'] ### percentage already covered by restaurants clean air laws
         pacb_set = scenario['pacb'] ### percentage already covered by bars clean air laws
         years_set = scenario['years'] ## Year of policy implementation
-#        policyrun(Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,directory)
         pool.apply_async(policyrun, args=(Iwp_set,Ir_set,Ib_set,pacwp_set,pacr_set,pacb_set,years_set,directory))
 
     pool.close() #closes the pool and prevents you from submitting any more jobs
--- scenarios_tcexp.py
+++ scenarios_tcexp.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import csv
 import sys
@@ -5,121 +7,142 @@ from itertools import product
 import multiprocessing as mp
 import time
 
-dirweb = '/home/jamietam/web-interface-shg-policy/'# Directory contains age eff$
+dirweb = '/home/jamietam/web-interface-shg-policy/'# Directory contains age effects files
 dirresults = '/home/jamietam/tcexp_results/'
 
 scenarioDict = {'0':{'initexp':[0.00],
-                      'finalexp':[0.10,0.20,0.30,0.40,0.50],
-                      'years':range(2016,2021)
-			}
-		'1':{'initexp':[0.00],
-                      'finalexp':[0.60,0.70,0.80,0.90,1.00],
-                      'years':range(2016,2021)
-			}
-		'2':{'initexp':[0.10],
-                      'finalexp':[0.20,0.30,0.40,0.50],
-                      'years':range(2016,2021)
-			}
-                '3':{'initexp':[0.10],
-                      'finalexp':[0.60,0.70,0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '4':{'initexp':[0.20],
-                      'finalexp':[0.30,0.40,0.50,0.60],
-                      'years':range(2016,2021)
-                        }
-                '5':{'initexp':[0.20],
-                      'finalexp':[0.70,0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '6':{'initexp':[0.30],
-                      'finalexp':[0.40,0.50,0.60,0.70],
-                      'years':range(2016,2021)
-                        }
-                '7':{'initexp':[0.30],
-                      'finalexp':[0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '8':{'initexp':[0.40],
-                      'finalexp':[0.50,0.60,0.70],
-                      'years':range(2016,2021)
-                        }
-                '9':{'initexp':[0.40],
-                      'finalexp':[0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '10':{'initexp':[0.50],
-                      'finalexp':[0.60,0.70,0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '11':{'initexp':[0.60],
-                      'finalexp':[0.70,0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '12':{'initexp':[0.70],
-                      'finalexp':[0.80,0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-                '13':{'initexp':[0.80,0.90],
-                      'finalexp':[0.90,1.00],
-                      'years':range(2016,2021)
-                        }
-		}
+                     'policyexp':[0.00,0.10,0.20,0.30],
+                     'years':[2016,2017,2018,2019,2020]
+                      },
+               '1':{'initexp':[0.00],
+                    'policyexp':[0.40,0.50,0.60,0.70],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '2':{'initexp':[0.00],
+                    'policyexp':[0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '3':{'initexp':[0.10],
+                    'policyexp':[0.20,0.30,0.40,0.50],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '4':{'initexp':[0.10],
+                    'policyexp':[0.60,0.70,0.80],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '5':{'initexp':[0.10],
+                    'policyexp':[0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '6':{'initexp':[0.20],
+                    'policyexp':[0.30,0.40,0.50,0.60],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '7':{'initexp':[0.20],
+                    'policyexp':[0.70,0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '8':{'initexp':[0.30],
+                    'policyexp':[0.40,0.50,0.60],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '9':{'initexp':[0.30],
+                    'policyexp':[0.70,0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '10':{'initexp':[0.40],
+                    'policyexp':[0.50,0.60,0.70],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '11':{'initexp':[0.40],
+                    'policyexp':[0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '12':{'initexp':[0.50],
+                    'policyexp':[0.60,0.70,0.80],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '13':{'initexp':[0.50],
+                    'policyexp':[0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '14':{'initexp':[0.60],
+                    'policyexp':[0.70,0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '15':{'initexp':[0.70],
+                    'policyexp':[0.80,0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '16':{'initexp':[0.80],
+                    'policyexp':[0.90,1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      },
+               '17':{'initexp':[0.90],
+                    'policyexp':[1.00],
+                    'years':[2016,2017,2018,2019,2020]
+                      }
+                }
 
+## CODE CURRENTLY SET UP TO DISTRIBUTE PARAMETER COMBOS TO EACH CORE, NOT EACH SCENARIO TO EACH CORE
 
-def policyrun (initexp_set,finalexp_set,years_set,directory):
-    combos = product(initexp_set,finalexp_set,years_set)
+def policyrun (initexp_set,policyexp_set,years_set,directory):
+    combos = product(initexp_set,policyexp_set,years_set)
     numscenarios = 0
-    for scen in list(combos):
-        dirsim = '/home/jamietam/shg-policy-module_parallel_{}/'.format(directory) # Directory contains 'policy_shg.py'
-        dirinputs = dirsim + 'inputs/' # Directory contains 'policies.csv' and 'demographics.csv'
-        # Create policy inputs file
-        os.chdir(dirweb)
-	os.system("Rscript create_tcexp_params_file.R %0.2f %0.2f %s" % scen)
+    combos_list=list(combos)
+    print "We have {} scenarios".format(len(combos_list))
+    for scen in combos_list:
+	dirsim = '/home/jamietam/shg-policy-module_parallel_{}/'.format(directory) # Directory contains 'policy_shg.py'
+	dirinputs = dirsim + 'inputs/' # Directory contains 'policies.csv' and 'demographics.csv'
+    	# Create policy inputs file
+    	os.chdir(dirweb)
+        os.system("Rscript create_tcexp_params_file.R %0.2f %0.2f %s" % scen)
 
-        ## Males
-	cmd1="mv inputstcexp_males_%0.2f_t%0.2f_%s.csv " % scen #move file to policy module inputs folder
-        cmd1=cmd1+dirinputs+"policies.csv"
-        os.system(cmd1)
-        os.chdir(dirinputs)
-        os.system("cp demographics_males_200000.csv demographics.csv")
-        os.chdir(dirsim)
+    	## Males
+    	cmd1="mv inputstcexp_males_initexp%0.2f_policyexp%0.2f_%s.csv " % scen # move file to policy module inputs folder
+    	cmd1=cmd1+dirinputs+"policies.csv"
+    	os.system(cmd1)
+    	os.chdir(dirinputs)
+    	os.system("cp demographics_males_200000.csv demographics.csv")
+    	os.chdir(dirsim)
 
-        runitM = "python policy_shg.py >> ../logM{0}.txt 2>> ../errorM{0}.txt".format(directory) ### NAME THESE AS THE SCENARIO
-        os.system(runitM) # run policy module
+    	runitM = "python policy_shg.py >> ../logM{0}.txt 2>> ../errorM{0}.txt".format(directory) ### NAME THESE AS THE SCENARIO
+    	os.system(runitM) # run policy module
 
-	cmd2="mv prevalences.csv "+dirresults+"prevalences_males_%0.2f_t%0.2f_%s.csv" % scen
-        os.system(cmd2)
+    	cmd2="mv prevalences.csv "+dirresults+"prevalences_males_initexp%0.2f_policyexp%0.2f_%s.csv" % scen
+    	os.system(cmd2)
 
-        ## Females
-        os.chdir(dirweb)
-	cmd3="mv inputstcexp_females_%0.2f_t%0.2f_%s.csv " % scen
-        cmd3=cmd3+dirinputs+"policies.csv"
-        os.system(cmd3)
-        os.chdir(dirinputs)
-        os.system("cp demographics_females_200000.csv demographics.csv")
-        os.chdir(dirsim)
+    	## Females
+    	os.chdir(dirweb)
+    	cmd3="mv inputstcexp_females_initexp%0.2f_policyexp%0.2f_%s.csv " % scen
+    	cmd3=cmd3+dirinputs+"policies.csv"
+    	os.system(cmd3)
+    	os.chdir(dirinputs)
+    	os.system("cp demographics_females_200000.csv demographics.csv")
+    	os.chdir(dirsim)
 
-        runitF = "python policy_shg.py >> ../logF{0}.txt 2>> ../errorF{0}.txt".format(directory)
-        os.system(runitF) # run policy module
-	cmd4="mv prevalences.csv "+dirresults+"prevalences_females_%0.2f_t%0.2f_%s.csv" % scen
-        os.system(cmd4)
-        numscenarios = numscenarios+1
+    	runitF = "python policy_shg.py >> ../logF{0}.txt 2>> ../errorF{0}.txt".format(directory)
+    	os.system(runitF) # run policy module
+	cmd4="mv prevalences.csv "+dirresults+"prevalences_females_initexp%0.2f_policyexp%0.2f_%s.csv" % scen
+    	os.system(cmd4)
+	numscenarios = numscenarios+1
     print ("directory " + str(directory)+" = " + str(numscenarios)+ " scenarios")
     print >>sys.stderr, "Done with all combos={}".format(directory)
     print scen
 
+# Run scenarios in parallel and serially within each thread
+
 if __name__ == '__main__':
     # cpus = mp.cpu_count()
-    pool = mp.Pool(processes=14)
+    pool = mp.Pool(processes=18)
     for key, scenario in scenarioDict.items():
         directory = key
         print("ITERATION: "+ directory)
 
         initexp_set = scenario['initexp']
-        finalexp_set = scenario['finalexp']
+        policyexp_set = scenario['policyexp']
         years_set = scenario['years'] ## Year of policy implementation
-        pool.apply_async(policyrun, args=(initexp_set,finalexp_set,years_set,directory))
+        pool.apply_async(policyrun, args=(initexp_set,policyexp_set,years_set,directory))
 
     pool.close() #closes the pool and prevents you from submitting any more jobs
     pool.join() # waits for all the jobs to finish before moving onto the next line of code

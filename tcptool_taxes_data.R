17ccb7123c5091cd406f73913f98ae67f7b6c7df
Jamie Tam <jamietam@umich.edu>
2018-11-06 21:26:58 -0500

    make tax runs include baseline and remove state loop

--- tcptool_taxes_data.R
+++ tcptool_taxes_data.R
@@ -5,18 +5,16 @@ library(reshape)
 library(data.table)
 
 setwd("/home/jamietam/web-interface-shg-policy/")
-prevfiles = '/home/jamietam/taxes_results/prevsFeb2018/'
-mainDir <- "/home/jamietam/source_data/"
+prevfiles = '/home/jamietam/taxes_results/prevsSept2018/'
+mainDir <- "/home/jamietam/source_dataAug2018/"
 inputsDir <- "/home/jamietam/web-interface-shg-policy/"
 
-#initprices <- c(4.00,4.50,5.00,5.50,6.00,6.50,7.00,7.50,8.00,8.50,9.00,9.50,10.00,10.50)
-#taxes <- c(1.00,1.50,2.00,2.50,3.00,3.50,4.00,4.50,5.00)
-
-initprices <- c(4.00) # baseline scenario
-taxes <- c(0.00)
+initprices <- c(4.00,4.50,5.00,5.50,6.00,6.50,7.00,7.50,8.00,8.50,9.00,9.50,10.00,10.50)
+taxes <- c(0.00,1.00,1.50,2.00,2.50,3.00,3.50,4.00,4.50,5.00)
 
 for (v1 in initprices) {
   for (v2 in taxes) {
+    if (v1>4.00 & v2==0) next # only run baseline scenario once
     initprice =as.numeric(v1)
     tax = as.numeric(v2)
 
@@ -39,22 +37,4 @@ system(paste0("mv deaths_*t*.csv ", mainDir,"US/taxes/deaths"))
 system(paste0("mv lyg_*_t*.csv ", mainDir,"US/taxes/lyg"))
 system(paste0("mv results_*_t*.csv ", mainDir,"US/taxes/results"))
 
-## Update main directory and run state-level functions
-
-
-source('state_files_taxes.R')
-
-## LOOP THROUGH AND GENERATE STATE LEVEL FILES
-
-#allstates <- c("AK", "AZ", "AR", "CA", "CO","CT", "DE", "DC","FL", "GA","HI","ID","IL","IN","IA","KS","KY","LA","ME$
-#"MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "$
-#"SD", "TN", "TX", "UT", "VT","VA", "WA","WV","WI", "WY" )
-
-allstates <- c("AL")
-
-for (i in c(1:length(allstates))){
-  createresultsfiles(allstates[i]) # generates the results file for the state specified using the createresultsfile$
-  createdeathsfiles(allstates[i]) # generates the deaths file for the state specified
- createlygfiles(allstates[i]) # generates the lyg file for the state specified
-}
-
+## NEXT STEP: Generate state-level files with state_files_taxes.R

8e69f57cf5fe7edd4b3ca4908fd7ffcc485a358a
Alexander <galexv@umich.edu>
2019-01-03 19:43:14 -0500

    Python3 changes, better separation of data directories
    This is a squashed commit of the following:
    
    commit 74c5160ac400d4cbaeba4040f2e342ffdd5f7b51
    Author: Alexander <galexv@umich.edu>
    Date:   Thu Jan 3 18:23:24 2019 -0500
    
        Renamed scenarios_mla script back to working version
    
    commit 96005229e5b9e6beb770f3ab05d15d7d9c6c10a0
    Author: Alexander <galexv@umich.edu>
    Date:   Thu Jan 3 18:01:49 2019 -0500
    
        Restored differences between working and *_test version of scenarios_mla.py
    
    commit f2d990338ca2f5ce981a5e14a46046d6d5b82be5
    Author: Alexander Gaenko <galexv@umich.edu>
    Date:   Thu Dec 6 13:56:30 2018 -0500
    
        Avoided potential file conflict, no need to create any dirs manually
    
    commit 4a8113fdbba3f5a4b870e85918797f1dc4080803
    Author: Alexander Gaenko <galexv@umich.edu>
    Date:   Thu Dec 6 11:04:37 2018 -0500
    
        Added commented-out debugging/analysis statements
    
    commit 9b5350f0a7575f3d708debf476fcd1cec8e16202
    Author: Alexander Gaenko <galexv@umich.edu>
    Date:   Thu Nov 8 16:23:53 2018 -0500
    
        Generally works (tested single process)
    
    commit 5cb8d6d916b1f253330b69bd403ebea6de04ce89
    Author: Alexander Gaenko <galexv@umich.edu>
    Date:   Thu Nov 8 13:41:37 2018 -0500
    
        Snapshot: work in progress
    
    commit da5906f11cf8bd397ea3f0fde23bf110a3f0aca0
    Author: Alexander <galexv@umich.edu>
    Date:   Thu Jan 3 16:39:10 2019 -0500
    
        Forcibly sync scenario_mla_test with the other branch
    
    commit 49b1aaa833e064479244c40146b4ba03293a05e5
    Author: Alexander <galexv@umich.edu>
    Date:   Thu Jan 3 16:33:57 2019 -0500
    
        Renamed script file to match the other branch

--- scenarios_mla.py
+++ scenarios_mla.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import csv
 import sys
@@ -7,145 +8,177 @@ import time
 
 cohortsize = 50
 lastcohort = 2060
-dirweb = '/home/jamietam/web-interface-shg-policy/'
-dirresults = '/home/jamietam/mla_results/'
 
-scenarioDict = {'0':{'mla_age':[19],
-                      'pac19':[0.00,0.25,0.50,0.75,1.00],
-                      'pac21':[0.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '1':{'mla_age':[19],
-                      'pac19':[0.00,0.25,0.50,0.75],
-                      'pac21':[0.25],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '2':{'mla_age':[19],
-                      'pac19':[0.00,0.25,0.50],
-                      'pac21':[0.50],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '3':{'mla_age':[19],
-                      'pac19':[0.00,0.25],
-                      'pac21':[0.75],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '4':{'mla_age':[19],
-                      'pac19':[0.00],
-                      'pac21':[1.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-        	'5':{'mla_age':[21],
-                      'pac19':[0.00,0.25,0.50,0.75,1.00],
-                      'pac21':[0.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '6':{'mla_age':[21],
-                      'pac19':[0.00,0.25,0.50,0.75],
-                      'pac21':[0.25],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '7':{'mla_age':[21],
-                      'pac19':[0.00,0.25,0.50],
-                      'pac21':[0.50],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '8':{'mla_age':[21],
-                      'pac19':[0.00,0.25],
-                      'pac21':[0.75],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '9':{'mla_age':[21],
-                      'pac19':[0.00],
-                      'pac21':[1.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-		'10':{'mla_age':[25],
-                      'pac19':[0.00,0.25,0.50,0.75,1.00],
-                      'pac21':[0.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '11':{'mla_age':[25],
-                      'pac19':[0.00,0.25,0.50,0.75],
-                      'pac21':[0.25],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '12':{'mla_age':[25],
-                      'pac19':[0.00,0.25,0.50],
-                      'pac21':[0.50],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '13':{'mla_age':[25],
-                      'pac19':[0.00,0.25],
-                      'pac21':[0.75],
-                      'years':[2016,2017,2018,2019,2020]
-                      },
-               '14':{'mla_age':[25],
-                      'pac19':[0.00],
-                      'pac21':[1.00],
-                      'years':[2016,2017,2018,2019,2020]
-                      }
-}
+this_dir=os.getcwd()
+dirweb = this_dir+'/inputdir'
+dirresults = this_dir+'/results'
+dirsim_base=this_dir+'/dirsim_'
+# policy_script_py="/usr/bin/time -a -o "+os.path.join(this_dir,"policy_script.py.timing")+" "+os.path.join(this_dir,'policy_shg.py')
+policy_script_py=os.path.join(this_dir,'policy_shg.py')
 
+create_mla_params_rscript='Rscript ' + this_dir + '/create_mla_params_file.R'
+csv_inputs_dir=this_dir + '/csv_inputs'
 
-def policyrun (mla_age_set,pac19_set,pac21_set,years_set,directory):
-    combos = product(mla_age_set,pac19_set,pac21_set,years_set)
+scenarioDict = {'0':{'mla_age':[19],                         
+                      'pac19':[0.00,0.25,0.50,0.75,1.00],    
+                      'pac21':[0.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '1':{'mla_age':[19],                          
+                      'pac19':[0.00,0.25,0.50,0.75],         
+                      'pac21':[0.25],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '2':{'mla_age':[19],                          
+                      'pac19':[0.00,0.25,0.50],              
+                      'pac21':[0.50],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '3':{'mla_age':[19],                          
+                      'pac19':[0.00,0.25],                   
+                      'pac21':[0.75],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '4':{'mla_age':[19],                          
+                      'pac19':[0.00],                        
+                      'pac21':[1.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+                '5':{'mla_age':[21],                         
+                      'pac19':[0.00,0.25,0.50,0.75,1.00],    
+                      'pac21':[0.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '6':{'mla_age':[21],                          
+                      'pac19':[0.00,0.25,0.50,0.75],         
+                      'pac21':[0.25],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '7':{'mla_age':[21],                          
+                      'pac19':[0.00,0.25,0.50],              
+                      'pac21':[0.50],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '8':{'mla_age':[21],                          
+                      'pac19':[0.00,0.25],                   
+                      'pac21':[0.75],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '9':{'mla_age':[21],                          
+                      'pac19':[0.00],                        
+                      'pac21':[1.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+                '10':{'mla_age':[25],                        
+                      'pac19':[0.00,0.25,0.50,0.75,1.00],    
+                      'pac21':[0.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '11':{'mla_age':[25],                         
+                      'pac19':[0.00,0.25,0.50,0.75],         
+                      'pac21':[0.25],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '12':{'mla_age':[25],                         
+                      'pac19':[0.00,0.25,0.50],              
+                      'pac21':[0.50],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '13':{'mla_age':[25],                         
+                      'pac19':[0.00,0.25],                   
+                      'pac21':[0.75],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      },                                     
+               '14':{'mla_age':[25],                         
+                      'pac19':[0.00],                        
+                      'pac21':[1.00],                        
+                      'years':[2016,2017,2018,2019,2020]     
+                      }                                         
+}                                                            
+
+def run_system(cmd):
+    """Run a command `cmd` via system(), raise exception if the return code is not zero"""
+    rc=os.system(cmd)
+    if rc!=0:
+        raise RuntimeError("Command '{}' terminated with return code {}".format(cmd,rc))
+    return rc
+
+def policyrun (mla_age_set,pac19_set,pac21_set,years_set,directory): 
+    combos = product(mla_age_set, pac19_set, pac21_set, years_set)
     numscenarios = 0
-    for scen in list(combos):
-        dirsim = '/home/jamietam/shg-policy-module_parallel_{}/'.format(directory) # Directory contains 'policy_shg.py'
-        dirinputs = dirsim + 'inputs/' # Directory contains 'policies.csv' and 'demographics.csv'
-        # Create policy inputs file
-        os.chdir(dirweb)
-        os.system("Rscript create_mla_params_file.R %s %0.2f %0.2f %s" % scen)
+    combos_list = list(combos)
+    print("DEBUG: We have {} scenarios".format(len(combos_list)))
+    for scen in combos_list:
+        dirsim = dirsim_base+directory+'/'
+        dirinputs = dirsim + 'inputs'
 
-        ## Males
-        cmd1="mv inputsmla_males_%s_pac19_%0.2f_pac21_%0.2f_%s.csv " % scen
-        cmd1=cmd1+dirinputs+"policies.csv"
-        os.system(cmd1)
-        demoM ="cp "+dirweb+"demographics/demographics_males_"+str(cohortsize)+"_" +str(lastcohort)+".csv "+dirinputs+"demographics.csv"
-        os.system(demoM)
+        run_system("mkdir -pv "+dirinputs)
+        os.chdir(dirinputs)
+
+        run_system(create_mla_params_rscript + ' %s %0.2f %0.2f %s' % scen)
+
+        cmd = "mv inputsmla_males_%s_pac19_%0.2f_pac21_%0.2f_%s.csv policies.csv" % scen
+        run_system(cmd)
+        cmd="cp {}/demographics_males_{}_{}.csv demographics.csv".format(csv_inputs_dir, cohortsize, lastcohort)
+        run_system(cmd)
+
         os.chdir(dirsim)
 
-        runitM = "python policy_shg.py >> ../logM{0}.txt 2>> ../errorM{0}.txt".format(directory) ### NAME THESE AS THE SCENARIO
-        os.system(runitM) # run policy module
+        runitM = policy_script_py +" >> ../logM{0}.txt 2>> ../errorM{0}.txt".format(directory)
+        ## DEBUG
+        #runitM = 'python3 -m pdb '+policy_script_py
 
-        cmd2="mv prevalences.csv "+dirresults+"prevalences_males_%s_pac19_%0.2f_pac21_%0.2f_%s.csv" % scen
-        os.system(cmd2)
+        # # DEBUG!!!
+        # raise RuntimeError("DEBUG: About to run {}".format(runitM))
+        run_system(runitM)
+        cmd = "mv prevalences.csv " + dirresults \
+            + "prevalences_males_%s_pac19_%0.2f_pac21_%0.2f_%s.csv" \
+            % scen
+        run_system(cmd)
+
 
         ## Females
-        os.chdir(dirweb)
-        cmd3="mv inputsmla_females_%s_pac19_%0.2f_pac21_%0.2f_%s.csv " % scen
-        cmd3=cmd3+dirinputs+"policies.csv"
-        os.system(cmd3)
-        demoF ="cp "+dirweb+"demographics/demographics_females_"+str(cohortsize)+"_" +str(lastcohort)+".csv "+dirinputs+"demographics.csv"
-        os.system(demoF)
+        os.chdir(dirinputs)
+        cmd = "mv inputsmla_females_%s_pac19_%0.2f_pac21_%0.2f_%s.csv %s/policies.csv" % (*scen,dirinputs)
+        run_system(cmd)
+        run_system("cp {}/demographics_females_{}_{}.csv demographics.csv".format(csv_inputs_dir,cohortsize,lastcohort))
         os.chdir(dirsim)
-
-        runitF = "python policy_shg.py >> ../logF{0}.txt 2>> ../errorF{0}.txt".format(directory)
-        os.system(runitF) # run policy module
-
+        
+        runitF = policy_script_py + " >> ../logF{0}.txt 2>> ../errorF{0}.txt".format(directory)
+        run_system(runitF) # run policy module
         cmd4="mv prevalences.csv "+dirresults+"prevalences_females_%s_pac19_%0.2f_pac21_%0.2f_%s.csv" % scen
-        os.system(cmd4)
+        run_system(cmd4)
         numscenarios = numscenarios+1
-    print ("directory " + str(directory)+" = " + str(numscenarios)+ " scenarios")
-    print >>sys.stderr, "Done with all combos={}".format(directory)
-    print scen
+    
+    print("directory " + str(directory)+" = " + str(numscenarios)+ " scenarios")
+    print("Done with all combos={}".format(directory), file=sys.stderr)
+    #print(scen)
+
+# cpus = mp.cpu_count()
 
 if __name__ == '__main__':
-    # cpus = mp.cpu_count()
-    pool = mp.Pool(processes=15)
+    do_parallel=False # Set to True to run in parallel (FIXME: make it an input argument)
+
+    pool=None 
+    if (do_parallel):
+        pool = mp.Pool(processes=15) # (FIXME: make it an input argument)
+    
     for key, scenario in scenarioDict.items():
         directory = key
         print("ITERATION: "+ directory)
-
-        mla_age_set = scenario['mla_age'] 
+        
+        mla_age_set = scenario['mla_age']
         pac19_set = scenario['pac19']
         pac21_set = scenario['pac21']
         years_set = scenario['years'] ## Year of policy implementation
-        pool.apply_async(policyrun, args=(mla_age_set,pac19_set,pac21_set,years_set,directory))
+        if pool:
+            pool.apply_async(policyrun, args=(mla_age_set, pac19_set,
+                                              pac21_set, years_set, directory))
+        else:
+            policyrun(mla_age_set,pac19_set,pac21_set,years_set,directory)
 
-    pool.close() #closes the pool and prevents you from submitting any more jobs
-    pool.join() # waits for all the jobs to finish before moving onto the next line of code
+    if pool:
+        pool.close() #closes the pool and prevents you from submitting any more jobs
+        pool.join() # waits for all the jobs to finish before moving onto the next line of code
 
 ## NEXT STEP: Generate TCP tool files at US-level with tcptool_mla_data.R
-
